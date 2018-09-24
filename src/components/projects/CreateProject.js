c2382aaf78d62643d56004d78d770e1dc21f4fe3
Susanta <inbox.susanta@gmail.com>
2018-09-24 21:34:14 +0530

    Created form for new project

---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class CreateProject extends Component {
+  state = {
+      title: '',
+      content: ''
+  }
+  handleChanage = (e) => {
+    //   console.log(e);
+    this.setState({
+        [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    //   console.log(e);
+    e.preventDefault();
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div className="container">
+          <form onSubmit={this.handleSubmit} className="white">
+              <h5 className="grey-text text-darken-3">Create New Project</h5>
+              <div className="input-field">
+                  <label htmlFor="title">Title</label>
+                  <input type="text" name="title" id="title" onChange={this.handleChanage} />
+              </div>
+              <div className="input-field">
+                  <label htmlFor="content">Project Content</label>
+                  <textarea id="content" className="materialize-textarea" onChange={this.handleChanage}></textarea>
+              </div>
+              <div className="input-field">
+                  <button className="btn pink lighten-1 z-depth-0">Create</button>
+              </div>
+          </form>
+      </div>
+    )
+  }
+}
+
+export default CreateProject
